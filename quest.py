c9ea3f1a27bb773476ff35046d037a489716affe
dlecrivain <81257506+dlecrivain@users.noreply.github.com>
2023-06-22 18:20:16 +0200

    Create quest.py

---
+++ quest.py
@@ -0,0 +1,13 @@
+def quest(number):
+    if number == 0 :
+        print("Bloc aventure 0")
+        input('Appuyer sur une touche pour continuer')
+        #number = 1
+        return "1"
+    elif number == 1 :
+        print("Bloc aventure 1")
+        input('Appuyer sur une touche pour continuer')
+        return "0"
+    else:
+        print("bizarre")
+
